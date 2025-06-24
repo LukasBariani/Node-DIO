b8237d00a57e8260ce76f0c10b8bde4dec19578b
Lukas Bariani <lukasbariani08@gmail.com>
2025-06-24 11:12:57 -0300

    add functions rolllDice, RandomBlock, engine and main

--- Node-23-06/src/index.js
+++ Node-23-06/src/index.js
@@ -22,6 +22,46 @@ async function rolaDado() {
     return dado
 }
 
+async function getRandomBloc(){
+    let random = Math.random()
+    let result 
+
+    switch (true) {
+        case random < 0.33:
+            result = "reta"
+            break;
+        case random < 0.66: 
+            result = "curva"
+            break;
+        default:
+            result ="confronto"
+            break;
+    }
+    return result
+}
+
+async function playRaceEngine(player1,player2) {
+    for(let round = 1; round <= 5; round++){
+        console.log(`rodada ${round}`)
+
+        //sortear bloco 
+        let bloc = await getRandomBloc()
+        console.log(`Bloco: ${bloc}  sorteado`)
+    }
+
+    //roll dice
+
+    let dice1 = await rolaDado()
+    let dice2 = await rolaDado()
+
+    let totalSkill1 = 0 
+    let roralSkill2 = 0 
+}
+
+
+
 (async function main() {
     console.log(`🏁🚦Começando entre ${mario.getName()} e ${luigi.getName()}...`)
+
+    await playRaceEngine(mario, luigi)
 })();
